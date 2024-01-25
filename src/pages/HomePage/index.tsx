2e0d385b204bdd4c0fa17cf61e99c40977aac8ae
Belka <belka.srh.it@gmail.com>
2024-01-25 08:51:29 +0100

    update: strusture

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,17 +1,9 @@
-import AboutUs from './AboutUs';
-import Advantages from './Advantages';
-import Customers from './Customers';
 import Hero from './Hero';
-import OurTeam from './OurTeam';
 
 const HomePage = () => {
   return (
     <>
       <Hero />
-      <Advantages />
-      <AboutUs />
-      <OurTeam />
-      <Customers />
     </>
   );
 };
--- src/utils/context/aboutusContext.js
+++
@@ -1,9 +0,0 @@
-import { getFiles } from './getFiles';
-
-const context = require.context(
-  '../../../src/assets/img/aboutus',
-  true,
-  /\.(png|jpe?g|svg)$/,
-);
-
-export default getFiles(context);
